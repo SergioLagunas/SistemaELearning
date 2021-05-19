fbce0283a3f62ccbd2f33bae7bde1ebdc90ef805
Jorge Adan <jorge.adan1906@gmail.com>
2021-05-19 17:27:25 -0500

    DTO - Tabla Evaluacion.

---
+++ src/main/java/Elearning/dto/Evaluacion.java
@@ -0,0 +1,51 @@
+package Elearning.dto;
+
+import java.sql.*;
+
+public class Evaluacion {
+    private Integer idEvaluacion;
+    private String tipo;
+    private float calificacion;
+    private String aprobacion;
+    private Modulo idModulo;
+
+    public Integer getIdEvaluacion() {
+        return idEvaluacion;
+    }
+
+    public void setIdEvaluacion(Integer idEvaluacion) {
+        this.idEvaluacion = idEvaluacion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(float calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getAprobacion() {
+        return aprobacion;
+    }
+
+    public void setAprobacion(String aprobacion) {
+        this.aprobacion = aprobacion;
+    }
+
+    public Modulo getIdModulo() {
+        return idModulo;
+    }
+
+    public void setIdModulo(Modulo idModulo) {
+        this.idModulo = idModulo;
+    }
+}
