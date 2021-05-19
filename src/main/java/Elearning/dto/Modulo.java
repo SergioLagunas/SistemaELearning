cc61286c1e69a8e9615137fe2e0bf3f060def83b
Said <Said@DESKTOP-MGI280U>
2021-05-19 17:19:49 -0500

    se creo el DTO de Modulo

---
+++ src/main/java/Elearning/dto/Modulo.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Elearning.dto;
+
+/**
+ *
+ * @author Said
+ */
+public class Modulo {
+    private Integer idModulo;
+    private String unidad;
+    private String leccion;
+    private TipoCurso  idTipoCurso;
+
+    public TipoCurso getIdTipoCurso() {
+        return idTipoCurso;
+    }
+
+    public void setIdTipoCurso(TipoCurso idTipoCurso) {
+        this.idTipoCurso = idTipoCurso;
+    }
+    
+        public Integer getIdModulo() {
+        return idModulo;
+    }
+
+    public void setIdModulo(Integer idModulo) {
+        this.idModulo = idModulo;
+    }
+    
+        public String getunidad() {
+        return unidad;
+    }
+
+    public void setunidad(String unidad) {
+        this.unidad = unidad;
+    }
+    
+        public String getleccion() {
+        return leccion;
+    }
+
+    public void setleccion(String leccion) {
+        this.leccion = leccion;
+    }
+    
+        public String getidTipoCurso() {
+        return idTipoCurso;
+    }
+
+    public void setidTipoCurso(String idTipoCurso) {
+        this.idTipoCurso = idTipoCurso;
+    }
+}
