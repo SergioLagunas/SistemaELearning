a0622234854532acb16701018afe3d67c0fff8c8
sergi <sergi@192.168.1.75>
2021-05-19 16:33:46 -0500

    Creacion UsuarioDto

---
+++ src/main/java/Elearning/dto/Usuario.java
@@ -0,0 +1,91 @@
+
+package Elearning.dto;
+
+
+public class Usuario {
+   
+    private Integer idUsuario;
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+    private String genero;
+    private String email;
+    private String contrasena;
+    private String tUsuario;
+    private String rfc;
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaPaterno() {
+        return aPaterno;
+    }
+
+    public void setaPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getaMaterno() {
+        return aMaterno;
+    }
+
+    public void setaMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String gettUsuario() {
+        return tUsuario;
+    }
+
+    public void settUsuario(String tUsuario) {
+        this.tUsuario = tUsuario;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+    
+    
+}
