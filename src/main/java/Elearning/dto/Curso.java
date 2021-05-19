20a1d33d0e23a043011a475dd4c20acf430875e6
Emilio Godinez <emilioalbertogoca@gmail.com>
2021-05-19 17:24:01 -0500

    Se crearon DTO Curso y DTO MisCursos

---
+++ src/main/java/Elearning/dto/Curso.java
@@ -0,0 +1,33 @@
+package Elearning.dto;
+
+public class Curso {
+
+    private Integer idCurso;
+    private String nombre;
+    private String descripcion;
+    
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(Integer idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdTipoCurso() {
+        return descripcion;
+    }
+
+    public void setIdTipoCurso(String idTipoCurso) {
+        this.descripcion = idTipoCurso;
+    }
+    
+}
---
+++ src/main/java/Elearning/dto/MisCursos.java
@@ -0,0 +1,33 @@
+package Elearning.dto;
+
+public class MisCursos {
+    private Integer IdMisCursos;
+    private Integer IdUsuario;
+    private TipoCurso idTipoCurso;
+
+    public Integer getIdMisCursos() {
+        return IdMisCursos;
+    }
+
+    public void setIdMisCursos(Integer IdMisCursos) {
+        this.IdMisCursos = IdMisCursos;
+    }
+
+    public Integer getIdUsuario() {
+        return IdUsuario;
+    }
+
+    public void setIdUsuario(Integer IdUsuario) {
+        this.IdUsuario = IdUsuario;
+    }
+
+    public Integer getIdTipoCurso() {
+        return idTipoCurso;
+    }
+
+    public void setIdTipoCurso(TipoCurso idTipoCurso) {
+        this.idTipoCurso = idTipoCurso;
+    }
+    
+    
+}
