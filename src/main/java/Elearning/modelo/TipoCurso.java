f68d0b1b07a7252109bdf965c957d09ac4575432
sergi <sergi@192.168.1.75>
2021-05-24 18:16:21 -0500

    Primeras Pruebas

--- src/main/java/Elearning/modelo/TipoCurso.java
+++ src/main/java/Elearning/modelo/TipoCurso.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import javax.persistence.Table;
 public class TipoCurso implements Serializable{
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="idTipoCurso")
     private Integer idTipoCurso;
     
--- src/main/java/Elearning/pruebas/pruebas.java
+++ src/main/java/Elearning/pruebas/pruebas.java
@@ -1,42 +1,29 @@
-
 package Elearning.pruebas;
 
 import Elearning.modelo.Usuario;
 import Elearning.util.HibernateUtil;
-import java.util.List;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 public class pruebas {
+
     public static void main(String[] args) {
 
-        Usuario usuario = new Usuario("Sergio","Lagunas","Garcia","Masculino","sergio@gmail.com","contrasena","semillero","sdasd");
-        Usuario usuario1 = new Usuario("Emilio","nose","nose","Femenino","Emilioo@gmail.com","contrasena","semillero","sdasd");
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // save the student objects
-            session.save(usuario);
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+
+            Usuario usuario = new Usuario("Sergio", "Lagunas", "Garcia", "Masculino", "sergio@gmail.com", "contrasena", "semillero", "sdasd");
+            Usuario usuario1 = new Usuario("Emilio","nose","nose","Femenino","Emilioo@gmail.com","contrasena","semillero","sdasd");      
+            //Usuario usuario1 = new Usuario("Sergio", "Lagunas", "Garcia", "Masculino", "sergio@gmail.com", "contrasena", "semillero", "sdasd")
+            session.beginTransaction();
             session.save(usuario1);
-            //commit transaction
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
+            session.beginTransaction().rollback();
+            System.out.println("Registro Insertado Correctamente");
+
+            session.close();
+
+        } finally {
+            session.close();
         }
 
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            List < Usuario > usuarios = session.createQuery("from Usuario",Usuario.class).list();
-           // usuarios.forEach(s -> System.out.println(s.));
-            //usuarios.forEach(s - > System.out.println(s.getNombre()));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
     }
 }
