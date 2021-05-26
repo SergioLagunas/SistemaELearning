9e79e3e14eea0d2074d8f454e1cf2c6b35f3221c
sergi <sergi@192.168.1.75>
2021-05-26 17:29:37 -0500

    UsuarioDao Listar

--- src/main/java/Elarning/dao/UsuarioDao.java
+++ src/main/java/Elarning/dao/UsuarioDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface UsuarioDao {
     
-    public List<Usuario> getUsuarios();
+    public List<Usuario> findAll();
     
     
 }
--- src/main/java/Elearning/dao/impl/UsuarioDaoImpl.java
+++ src/main/java/Elearning/dao/impl/UsuarioDaoImpl.java
@@ -1,29 +1,50 @@
-
 package Elearning.dao.impl;
 
 import Elarning.dao.UsuarioDao;
 import Elearning.modelo.Usuario;
 import Elearning.util.HibernateUtil;
 import java.util.List;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UsuarioDaoImpl implements UsuarioDao{
+public class UsuarioDaoImpl implements UsuarioDao {
 
     @Override
-    public List<Usuario> getUsuarios() {
-        
+    public List<Usuario> findAll() {
         //Obtener la secion 
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        
-        //crea la consulta Query
-        Query<Usuario> miQuery = session.createQuery("from Usuario",Usuario.class);
-      //  List<Usuario> 
-      
-      return null;
+        //Ocupamos la transaccion en caso de error la base de datos se restaura a como estaba
+        Transaction transaccion = session.getTransaction();
+        //Declaramos la lista donde almacenara el conjunto de datos de la tabla 
+        List<Usuario> lista = null;
+
+        try {
+            //Iniciamos Transaccion
+            transaccion.begin();
+            //crea la consulta Query
+            Query<Usuario> miQuery = session.createQuery("from Usuario", Usuario.class);
+            //Amacenamos los datos en la lista declarada anteriormente 
+            lista = miQuery.list();
+            //regresa el commit
+            transaccion.commit();
+        } catch (HibernateException e) {
+            //Si la transaccion esta bacia y ademas esta activa que regrese el estado en el que se encontraba la Base de Dato
+            if (transaccion != null && transaccion.isActive()) {
+                transaccion.rollback();
+            }
+        } finally {
+            //Finalmente cerramos la sesion 
+            session.close();
+        }
+        return lista;
     }
     
-   
+    
+    
+    
+
 }
