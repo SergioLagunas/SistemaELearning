3dc12974a53878d5c6cb2a7646f3d0da02006f19
sergi <sergi@192.168.1.75>
2021-06-30 15:42:55 -0500

    dependencias

--- src/main/java/Elearning/controler/HomeController.java
+++ src/main/java/Elearning/controler/HomeController.java
@@ -5,9 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+
 public class HomeController {
     
     @RequestMapping(value = "index.html")
